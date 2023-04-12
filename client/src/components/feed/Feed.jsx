da740e28298ddc97e11bd4fbd37bd84a5adf44e6
ayush basnet <basnetayush184@gmail.com>
2023-04-12 11:55:58 +0530

    first commit

---
+++ client/src/components/feed/Feed.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
+
+export default function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/642a708c03763b02f8ca9e6a");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
